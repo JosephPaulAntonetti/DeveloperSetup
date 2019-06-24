5b28bd13f59034ff75d852da5eda8eddbcd96499
Joe Antonetti <joeantonetti@joes-mbp.lan>
2019-06-23 19:33:07 -0700

    Install VIM via brew when installing

--- aliases/common.sh
+++ aliases/common.sh
@@ -1,3 +1,5 @@
 export DEV=~/dev
 
+# VIM Aliases
 alias vi=nvim
+alias viupdate='nvim +PluginInstall +qall'
--- install/setup_vim.sh
+++ install/setup_vim.sh
@@ -1,2 +1,4 @@
+brew install nvim
 mkdir -p ~/.config/nvim
 ln -s ~/dev/init.vim ~/.config/nvim/init.vim
+nvim ~PluginInstall +qall
